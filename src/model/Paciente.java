8576651c59b0857bf4dc9fd7a7b1af4653762acc
Giovani303 <79032448+Giovani303@users.noreply.github.com>
2026-08-26 23:54:27 -0300

    Update Paciente.java

--- src/model/Paciente.java
+++ src/model/Paciente.java
@@ -1,4 +1,18 @@
 package model;
 
-public class Paciente {
+
+public record Paciente(String nome, int idade, NivelEmergencia nivel, boolean possuiPlano) {
+
+
+    public Paciente {
+        if (nome == null || nome.isBlank()) {
+            throw new IllegalArgumentException("O nome do paciente não pode ser nulo ou vazio.");
+        }
+        if (idade < 0) {
+            throw new IllegalArgumentException("A idade do paciente não pode ser negativa.");
+        }
+        if (nivel == null) {
+            throw new IllegalArgumentException("O nível de emergência é obrigatório.");
+        }
+    }
 }
